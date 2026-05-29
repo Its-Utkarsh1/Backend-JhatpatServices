c0ac571530ed84c172162aad90f187baaf23e2ff
Utkarsh <utkarsh.sri.9170@gmail.com>
2026-05-29 19:06:01 +0530

    Update OtpService.java

--- src/main/java/com/example/demo/Service/OtpService.java
+++ src/main/java/com/example/demo/Service/OtpService.java
@@ -1,40 +1,32 @@
 package com.example.demo.Service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.time.Duration;
 import java.util.Random;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 @RequiredArgsConstructor
 public class OtpService {
 
     private final Map<String, String> otpStore = new ConcurrentHashMap<>();
-    private static final long OTP_EXPIRY = 5;
-    private static final String OTP_PREFIX = "otp:";
 
-
-    public String genetateOtp(String email){
+    public String genetateOtp(String email) {
         String otp = String.format("%06d", new Random().nextInt(999999));
-        String key = OTP_PREFIX + email;
         otpStore.put(email, otp);
-
         return otp;
     }
 
-    public boolean verifyotp(String email, String inputOtp){
-       String storedOtp = otpStore.get(email);
+    public boolean verifyotp(String email, String inputOtp) {
+        String storedOtp = otpStore.get(email);
 
-        if (storedOtp != null && storedOtp.equals(otp)) {
+        if (storedOtp != null && storedOtp.equals(inputOtp)) {
             otpStore.remove(email);
             return true;
         }
 
-        return false;;
+        return false;
     }
-
-
-
 }
